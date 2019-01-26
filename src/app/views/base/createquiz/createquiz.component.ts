ffeb5bc442ecbe6d18a021b0813d646cfc1d03c4
italhatk <mail.talha.tk@gmail.com>
2019-01-26 12:49:24 +0500

    Create Quizz page in progress

--- src/app/views/base/createquiz/createquiz.component.ts
+++ src/app/views/base/createquiz/createquiz.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-createquiz',
-  templateUrl: './createquiz.component.html',
+  selector: "app-createquiz",
+  templateUrl: "./createquiz.component.html",
   styles: []
 })
 export class CreatequizComponent implements OnInit {
+  max: number;
+  rate: number;
+  isReadonly: boolean;
 
-  constructor() { }
+  overStar: number | undefined;
+  percent: number;
 
-  ngOnInit() {
+  isCollapsed: boolean;
+  iconCollapse: string;
+
+  constructor() {
+    this.max = 10;
+    this.rate = 7;
+    this.isReadonly = false;
+    this.isCollapsed = false;
+    this.iconCollapse = "icon-arrow-up";
   }
 
+  ngOnInit() {}
+
+  // Rating Code Start
+  // Rating Code Start
+  // Rating Code Start
+
+  hoveringOver(value: number): void {
+    this.overStar = value;
+    this.percent = (value / this.max) * 100;
+  }
+
+  resetStar(): void {
+    this.overStar = void 0;
+  }
+  // Rating Code End
+  // Rating Code End
+  // Rating Code End
+  // Rating Code End
+
+  collapsed(event: any): void {
+    // console.log(event);
+  }
+
+  expanded(event: any): void {
+    // console.log(event);
+  }
+
+  toggleCollapse(): void {
+    this.isCollapsed = !this.isCollapsed;
+    this.iconCollapse = this.isCollapsed ? "icon-arrow-down" : "icon-arrow-up";
+  }
 }
