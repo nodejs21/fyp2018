1a7923cccfbc114c3c72b4be2b41ae4c24a19811
nodejs21 <nodejs21@gmail.com>
2019-02-03 12:41:26 +0500

    ✔ a little misroute in account-routing.module.ts - fixed

--- src/app/components/account/account-routing.module.ts
+++ src/app/components/account/account-routing.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
-import { ProfileComponent } from '../shared/profile/profile.component';
 import { AccountComponent } from './account.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'profile', pathMatch: 'full'},
   {path: '', component: AccountComponent, children: [
-    {path: 'profile', component: ProfileComponent},
+    // {path: 'profile', component: ProfileComponent},
     {path: 'signup', component: SignupComponent},
     {path: 'reset-password', component: ResetpasswordComponent},
   ]},
