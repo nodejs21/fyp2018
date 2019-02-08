7cbfc52266fa9aee34484c8558bb6cccb832c81b
italhatk <mail.talha.tk@gmail.com>
2019-02-08 17:15:46 +0500

    routing files added

---
+++ src/app/components/users/teacher/teacher-routing.module.ts
@@ -0,0 +1,78 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ClassroomsComponent } from './classrooms/classrooms.component';
+import { AssignmentsComponent } from './course/assignments/assignments.component';
+import { AttendanceComponent } from './course/attendance/attendance.component';
+import { QuizzesComponent } from './course/quizzes/quizzes.component';
+import { ResultComponent } from './course/result/result.component';
+import { AcademydetailComponent } from './details/academydetail/academydetail.component';
+import { StudentdetailComponent } from './details/studentdetail/studentdetail.component';
+import { LecturesComponent } from './lectures/lectures.component';
+import { VideostreamComponent } from './liveclass/videostream/videostream.component';
+import { WhiteboardComponent } from './liveclass/whiteboard/whiteboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { InvitationsComponent } from './requests/invitations/invitations.component';
+import { RequestssentComponent } from './requests/requestssent/requestssent.component';
+import { TeacherdashboardComponent } from './teacherdashboard/teacherdashboard.component';
+import { MakequizComponent } from './makequiz/makequiz.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+    children: [
+      { path: 'classrooms', component: ClassroomsComponent },
+      {
+        path: 'course',
+        redirectTo: 'assignments',
+        pathMatch: 'full',
+        children: [
+          { path: 'assignments', component: AssignmentsComponent },
+          { path: 'attendance', component: AttendanceComponent },
+          { path: 'quizzes', component: QuizzesComponent },
+          { path: 'results', component: ResultComponent }
+        ]
+      },
+      {
+        path: 'details',
+        redirectTo: 'assignments',
+        pathMatch: 'full',
+        children: [
+          { path: 'academydetail', component: AcademydetailComponent },
+          { path: 'studentdetail', component: StudentdetailComponent }
+        ]
+      },
+      { path: 'lectures', component: LecturesComponent },
+      {
+        path: 'liveclass',
+        redirectTo: 'videostream',
+        pathMatch: 'full',
+        children: [
+          { path: 'videostream', component: VideostreamComponent },
+          { path: 'whiteboard', component: WhiteboardComponent }
+        ]
+      },
+      { path: 'makequiz', component: MakequizComponent },
+      { path: 'profile', component: ProfileComponent },
+      {
+        path: 'requests',
+        redirectTo: 'invitations',  
+        pathMatch: 'full',
+        children: [
+          { path: 'invitations', component: InvitationsComponent },
+          { path: 'requestssent', component: RequestssentComponent }
+          ]
+      },
+      { path: 'dashboard', component: TeacherdashboardComponent }
+    ]
+  },
+
+  { path: '**', redirectTo: 'profile', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TeacherRoutingModule {}
