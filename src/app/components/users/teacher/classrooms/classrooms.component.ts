9aa71d3fad52eb4610ff3c78f1649f68ef3db8dd
italhatk <mail.talha.tk@gmail.com>
2019-04-23 14:51:33 +0500

    ✔ Partial Notification added for teacher annoucements

--- src/app/components/users/teacher/classrooms/classrooms.component.ts
+++ src/app/components/users/teacher/classrooms/classrooms.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { AuthService } from "../../../../utils/services/auth/auth.service";
 
 @Component({
-  selector: 'app-classrooms',
-  templateUrl: './classrooms.component.html',
-  styleUrls: ['./classrooms.component.scss']
+  selector: "app-classrooms",
+  templateUrl: "./classrooms.component.html",
+  styleUrls: ["./classrooms.component.scss"]
 })
 export class ClassroomsComponent implements OnInit {
+  constructor(private _afs: AngularFirestore, private _auth: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  addAnnouncement() {
+    // this._afs.collection('notifications').add({title: 'check', body:'notification', token: 'students_token'})
   }
-
 }
